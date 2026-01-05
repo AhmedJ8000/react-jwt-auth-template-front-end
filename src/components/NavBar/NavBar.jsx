c95449b433847d97bcdc7e485811e5d51c8f9a77
AhmedJ8000 <ahmedjamal8000@gmail.com>
2026-01-05 08:39:41 +0300

    Added sign up, sign in, user context, and services

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,36 @@
-// src/components/NavBar/NavBar.jsx
+import { useContext } from 'react';
+import { Link } from 'react-router';
+
+import { UserContext } from '../../contexts/UserContext';
 
 const NavBar = () => {
+  const { user, setUser } = useContext(UserContext);
+
+  const handleSignOut = () => {
+    localStorage.removeItem('token');
+    setUser(null);
+  };
+
+  // The nav bar gets the user from the context which is either
+  // {username, _id} if logged in or null if not, and shows
+  // set of the correct set of links
   return (
     <nav>
-      <p>Navbar contents will go here.</p>
+      {user ? (
+        <ul>
+          <li>Welcome, {user.username}</li>
+          <li><Link to='/'>Dashboard</Link></li>
+          <li><Link to='/' onClick={handleSignOut}>Sign Out</Link></li>
+        </ul>
+      ) : (
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/sign-in'>Sign In</Link></li>
+          <li><Link to='/sign-up'>Sign Up</Link></li>
+        </ul>
+      )}
     </nav>
   );
 };
 
 export default NavBar;
-
